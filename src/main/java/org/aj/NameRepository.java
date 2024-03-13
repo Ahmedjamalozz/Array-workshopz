32aceeb4f97b57f5be94fd58fee39b09836a9c4f
Ahmed Jamal <142943974+Ahmedjamalozz@users.noreply.github.com>
2024-03-13 13:56:37 +0100

    Initial commit

---
+++ src/main/java/org/aj/NameRepository.java
@@ -0,0 +1,27 @@
+package org.aj;
+
+public class NameRepository {
+    private static String[] names = {"Ahmed Jamal","Jamal Adan"};
+
+    public static String find(final String fullName){
+        return null;
+    }
+
+
+    public static boolean add(final String fullName){
+        return false;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
